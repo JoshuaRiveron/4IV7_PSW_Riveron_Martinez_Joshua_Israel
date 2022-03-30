bd7cb49b001cd6d9c44c85234a13c223b3520c0d
JoshuaRiveron <jroveronm2000@alumno.ipn.mx>
2022-03-29 21:53:02 -0600

    Ejemplo bd

--- 09Servlet/src/java/Parametros.java
+++ 09Servlet/src/java/Parametros.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 import java.io.IOException;
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Compaq
+ * @author Alumno
  */
 public class Parametros extends HttpServlet {
 
@@ -28,20 +29,46 @@ public class Parametros extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
+        try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             
             String nombre = "";
+            int telefono = 0;
+            String correo = "";
+            
+            String ip, nombremaquina, ip2;
+            int puerto;
             
             nombre = request.getParameter("nombre");
-                    
+            telefono = Integer.parseInt(request.getParameter("telefono"));
+            correo = request.getParameter("correo");
+            
+            ip = request.getLocalAddr();
+            puerto = request.getLocalPort();
+            nombremaquina = request.getLocalName();
+            ip2 = request.getRemoteAddr();
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet Parametros</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Hola Mundo, tu nombre es: "+nombre+" </h1>");
+            out.println("<h1>Hola Mundo, tu nombre es: "+nombre+"</h1>"
+                    + "<br>"
+                    + "<h1>Tu telefono es: "+telefono+" </h1>"
+                    + "<br>"
+                    + "<h1>Tu Correo es: "+correo+" </h1>"
+                    + "<br>"
+                    + "<h1>El nombre de la maquina es: "+nombremaquina+" </h1>"
+                    + "<br>"
+                    + "<h1>IP local(servidor) es: "+ip+" </h1>"
+                    + "<br>"
+                    + "<h1>El puerto es: "+puerto+" </h1>"
+                    + "<br>"
+                    + "<h1>IP remota es: "+ip2+" </h1>"
+                    + "<br>"
+                    + "<a href = 'index.html'>Regresar al Principal</a>");
             out.println("</body>");
             out.println("</html>");
         }
